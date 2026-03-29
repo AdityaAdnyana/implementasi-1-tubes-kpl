4f0920ce10b45965adc99efa663b8b29da0f68cd
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-03-29 22:38:53 +0800

    feat: commiting first prototype of generic implementation

---
+++ IQuestion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplemantasiGenericQuiz
+{
+    internal interface IQuestion
+    {
+        public string QuestionText { get; set; }
+        public bool ValidateAnswer(object answer);
+    }
+}
---
+++ Lesson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplemantasiGenericQuiz
+{
+    internal class Lesson
+    {
+
+        public List<IQuestion> questions { get; set; } = new List<IQuestion>();
+       
+        public void StartSession()
+        {
+            //SessionManager sessionManager = SessionManager.Instance;
+
+            //sessionManager.InitializeNewSession(this);
+
+            //sessionManager.ChangeState(SessionState.PresentingQuestion);
+
+            Console.WriteLine("START SESSION");
+            foreach (var question in questions)
+            {
+                Console.WriteLine(question.QuestionText);
+                var userAnswer = Console.ReadLine();
+                bool isCorrect = question.ValidateAnswer(userAnswer);
+
+                if (isCorrect) Console.WriteLine("JAWABAN BENAR!");
+                else Console.WriteLine("JAWABAN SALAH");
+            }
+        }
+    }
+
+    
+}
